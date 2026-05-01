8278a0f6833ccc78e7506695bd30102d9d891c76
admin <yogeshrojekar88@gmail.com>
2026-05-01 10:26:29 +0530

    notepad firebase

---
+++ app/src/main/java/com/example/notepadfirebase/Task.java
@@ -0,0 +1,30 @@
+package com.example.notepadfirebase;
+
+public class Task {
+    private String id;
+    private String name;
+    private String email;
+
+
+    public Task() {
+        // Required for Firebase
+    }
+
+    public Task(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
